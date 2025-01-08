5c218e86de6559bd0dbae434f5856df955ec0721
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2025-01-08 01:58:19 -0300

    Add section + form input search

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,21 @@
 import React from "react";
-import GuerraNasEstrelasLogo from './components/Logo'
+import GuerraNasEstrelasLogo from "./components/Logo";
 
 export default function Home() {
   return (
-    <div >
-
+    <div>
       <main className="divMain">
-
         <div className="logo">
           <GuerraNasEstrelasLogo />
         </div>
-        <section>SEARCH BAR</section>
+        <section className="searchInput">
+          <form>
+            <input placeholder="Search Chracter..." type="text"></input>
+            <button>Search</button>
+          </form>
+        </section>
         <section>CARD + NOMES E INFORMAÇÕES</section>
-
       </main>
-
     </div>
   );
 }
